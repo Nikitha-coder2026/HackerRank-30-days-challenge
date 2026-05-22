acfef522061e53ea4e6a1b15b8ec6e8e8dcb8548
Nikitha-coder2026 <nikitha.r.codes@gmail.com>
2026-05-22 21:51:21 +0530

    Day 1 complete

---
+++ day1.py
@@ -0,0 +1,23 @@
+# ---------HackerRank-30-days-challenge---------
+# --------Day01: Data Types --------
+
+i = 4
+d = 4.0
+s = 'HackerRank'
+
+# Declare second integer, double, and string variables.
+# Read and save an integer, double, and string to your variables.
+
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+
+# Print the sum of the double variable on a new line.
+print(d + d2)
+
+# Concatenate and print the string variables on a new line.
+# The 's' variable above should be printed first.
+print(s + s2)
