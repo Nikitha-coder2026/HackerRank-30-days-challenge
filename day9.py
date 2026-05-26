1caeeeeb27eda470e11c1f46d3248ba8d17aae5b
Nikitha-coder2026 <nikitha.r.codes@gmail.com>
2026-05-26 20:40:26 +0530

    Day 9 complete

---
+++ day9.py
@@ -0,0 +1,26 @@
+# -------------HackerRank 30 day challenge-------------
+# -------------Day09: Recursion 3----------------------
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    result = factorial(n)
+    print(f"Factorial of {n} is: {result}")
